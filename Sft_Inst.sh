ab9703c34bf5b4634ef17c18c1ad4d0e06d0f07c
IHoggan <33704736+ihoggan@users.noreply.github.com>
2019-09-29 16:38:57 +0100

    Create Sft_Inst.sh

---
+++ Sft_Inst.sh
@@ -0,0 +1,19 @@
+apt-get install nmap -y
+apt-get remove vim-tiny -y
+apt-get install vim -y
+apt-get install youtube-dl -y
+apt-get install okular -y
+apt-get install gnome-schedule -y
+apt-get install lynx -y
+apt-get install links -y
+apt-get install filezilla -y
+apt-get install dnsutils -y
+apt-get install lsof -y
+
+pip3 install requests
+pip3 install bs4
+pip3 install python_nmap
+pip3 install scapy
+
+apt-get update
+apt-get upgrade -y
